948389f1b82d9d0a438b05b38d8f6313bcb78395
Raul Luna <mceclipse1889@gmail.com>
2025-07-07 19:57:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./style.css";
 import Header from "./Header.jsx";
+import Hero from "./Hero.jsx";
+import AnimatedMovies from "./AnimatedMovies.jsx";
+import Footer from "./Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Header/>
+        <Hero/>
+        <AnimatedMovies/>
+        <AnimatedMovies/>
+        <AnimatedMovies/>
+        <Footer/>
     </StrictMode>,
-)
+);
