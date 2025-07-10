6eac75e3bf34cc15005bdb55ba386ff9c73879b7
Raul Luna <mceclipse1889@gmail.com>
2025-07-10 18:22:37 -0500

    Add files via upload

---
+++ src/Category.jsx
@@ -0,0 +1,29 @@
+function Category() {
+    const CATEGORY = `bg-slate-800 text-slate-400 text-xs font-[600] inline-block px-3 py-2 rounded-[3rem] mr-2 mb-2`;
+    const LINE_CLAMP2 = {
+        overflow: "hidden",
+        display: "-webkit-box",
+        WebkitBoxOrient: "vertical",
+        WebkitLineClamp: 2,
+        whiteSpace: "normal",
+    }
+    
+    return (
+        <section className="mt-[2.5rem] flex flex-col">
+            <h1 className="mx-[2rem] text-sm font-[650]">Category</h1>
+            <div className="ml-[2rem] mt-[1rem] flex items-center gap-1 overflow-x-auto whitespace-nowrap 
+            scrollbar-hide">
+                <p className={CATEGORY}>All</p>
+                <p className={CATEGORY}>Drama</p>
+                <p className={CATEGORY}>Crime</p>
+                <p className={CATEGORY}>Action</p>
+                <p className={CATEGORY}>Romance</p>
+                <p className={CATEGORY}>Sci-Fi</p>
+                <p className={CATEGORY}>Horror</p>
+                <p className={CATEGORY}>Adventure</p>
+            </div>
+        </section>
+    );
+}
+
+export default Category;
