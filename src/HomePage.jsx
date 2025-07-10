ac86e303f5bd1f20a0d853edd527b601452556e7
Raul Luna <mceclipse1889@gmail.com>
2025-07-09 20:51:26 -0500

    Add files via upload

---
+++ src/HomePage.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header.jsx";
+import Hero from "./Hero.jsx";
+import AnimatedMovies from "./AnimatedMovies.jsx";
+import Footer from "./Footer.jsx";
+
+function HomePage() {
+    return (
+        <>
+            <Header/>
+            <Hero/>
+            <AnimatedMovies/>
+            <AnimatedMovies/>
+            <AnimatedMovies/>
+            <Footer/>
+        </>
+    );
+}
+
+export default HomePage;
