67e60b58e0432d9048e7d89c5727cbea22cb1ef9
Raul Luna <mceclipse1889@gmail.com>
2025-07-11 02:10:48 -0500

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,10 @@
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
 import AnimatedMovies from "./AnimatedMovies.jsx";
+import Category from "./Category.jsx";
+import ActionMovies from "./ActionMovies.jsx";
+import DramaMovies from "./DramaMovies.jsx";
+import Shows from "./Shows.jsx";
 import Footer from "./Footer.jsx";
 
 function HomePage() {
@@ -9,8 +13,10 @@ function HomePage() {
             <Header/>
             <Hero/>
             <AnimatedMovies/>
-            <AnimatedMovies/>
-            <AnimatedMovies/>
+            <Category/>
+            <ActionMovies/>
+            <DramaMovies/>
+            <Shows/>
             <Footer/>
         </>
     );
