a2f2a03359fe3ff852ad5e1fdaf23d4acf919eb5
Raul Luna <mceclipse1889@gmail.com>
2025-07-19 01:42:25 -0500

    Update SciFi.jsx

--- src/SciFi.jsx
+++ src/SciFi.jsx
@@ -48,7 +48,7 @@ function SciFi() {
                 );
                 setMoviesData(RESULTS);
             } catch (err) {
-                console.error("Failed to fetch movies:", err);
+                console.error("Failed to fetch animated movies:", err);
             }
         };
 
@@ -56,28 +56,36 @@ function SciFi() {
     });
 
     return (
-        <section className="mt-[2.5rem] flex flex-col">
-            <h1 className="mx-[2rem] text-sm font-[650]">Science Fiction</h1>
-            <div className="ml-[2rem] flex gap-5 overflow-x-auto whitespace-nowrap custom-scrollbar">
+        <section className="mt-[2.5rem] flex flex-col xl:mt-[3.5rem]">
+            <h1 className="mx-[2rem] text-sm font-[650] md:text-base lg:text-lg 
+            xl:text-xl xl:mb-[1rem] 2xl:ml-[3.4rem]">Science Fiction</h1>
+            <div className="ml-[2rem] flex gap-5 overflow-x-auto whitespace-nowrap 
+            custom-scrollbar lg:gap-7 xl:gap-9 2xl:ml-[3.4rem]">
                 {moviesData.map((movie) => (
                     <Link to={`/${movie.type}/${movie.id}`} key={movie.id} 
                     className="mt-[1rem] flex-shrink-0">
-                        <div className="mb-[.5rem] w-[5rem] h-[7rem] bg-slate-800 rounded-[1rem] overflow-hidden
-                        border-[#ffffff00] border-[2.5px] hover:border-[#b71234] transition-colors duration-[.25s]">
+                        <div className="mb-[.5rem] w-[5rem] h-[7rem] bg-slate-800 
+                        rounded-[1rem] overflow-hidden border-[#ffffff00] border-[2.5px]
+                        hover:border-[#b71234] transition-colors duration-[.25s]
+                        md:w-[6rem] md:h-[8.5rem] lg:w-[7rem] lg:h-[10rem] lg:mb-[.7rem] 
+                        xl:mb-[1rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
                             {movie.poster_path ? (
                                 <img src={`${TMDB_IMAGE_BASE_URL}${movie.poster_path}`} 
                                 alt={movie.title} className="w-full h-full object-cover" 
                                 draggable="false"/>
                             ) : (
-                                <div className="w-full h-full flex items-center justify-center text-xs text-slate-400 text-center p-1">
+                                <div className="w-full h-full flex items-center 
+                                justify-center text-xs text-slate-400 text-center p-1">
                                     No Poster
                                 </div>
                             )}
                         </div>
-                        <h3 style={LINE_CLAMP2} className="text-xs text-slate-400 font-[600] mb-[.25rem] w-[5rem]">
+                        <h3 style={LINE_CLAMP2} className="text-xs text-slate-400 
+                        font-[600] mb-[.25rem] w-[5rem] md:w-[6rem] lg:w-[7rem] 
+                        xl:w-[8rem] 2xl:w-[9rem] 2xl:text-sm">
                             {movie.title || movie.name}
                         </h3>
-                        <p className="text-xs text-slate-600 font-[600]">
+                        <p className="text-xs text-slate-600 font-[600] 2xl:text-sm">
                             {movie.vote_average?.toFixed(1) || "N/A"}
                         </p>
                     </Link>
