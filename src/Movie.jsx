6c8d8117d7d6a782ff763b93b052d8277b4fe93c
Raul Luna <mceclipse1889@gmail.com>
2025-07-20 23:23:35 -0500

    Update Movie.jsx

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -27,8 +27,8 @@ function Movie() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const GENRES = `bg-slate-800 text-slate-400 text-xs font-[600] inline-block px-3 py-2 rounded-[3rem] mr-2 mb-2
-    md:text-sm lg:text-base`;
+    const GENRES = `bg-slate-800 text-slate-400 text-xs font-[600] inline-block px-3 py-2 
+    rounded-[3rem] mr-2 mb-2 md:text-sm lg:text-base`;
 
     const LINE_CLAMP3 = {
         overflow: "hidden",
@@ -96,8 +96,11 @@ function Movie() {
     if (error) {
         return (
             <div className="flex flex-col items-center justify-center h-[100vh]">
-                <p className="text-base text-[#b71234] font-[650] text-center mb-6 w-[15ch]">{error}</p>
-                <button onClick={() => navigate(-1)} className="p-2 rounded-full bg-slate-800 hover:bg-slate-900 transition-colors duration-[.25s] flex items-center justify-center">
+                <p className="text-base text-[#b71234] font-[650] text-center mb-6 
+                w-[15ch]">{error}</p>
+                <button onClick={() => navigate(-1)} className="p-2 rounded-full 
+                bg-slate-800 hover:bg-slate-900 transition-colors duration-[.25s] flex 
+                items-center justify-center">
                     <img className="w-[1.5rem] h-[1.5rem]" src={back} alt="Back Icon" draggable="false" />
                 </button>
             </div>
@@ -112,55 +115,79 @@ function Movie() {
         <div>
             <Header/>
             <div className="relative bg-black mb-4">
-                <button onClick={() => navigate(-1)} className="absolute z-8 ml-[1rem] mt-[1rem] p-2 rounded-full bg-slate-800 hover:bg-slate-900 transition-colors duration-[.25s] flex items-center justify-center">
-                    <img className="w-[1.5rem] h-[1.5rem]" src={back} alt="Back Icon" draggable="false"/>
+                <button onClick={() => navigate(-1)} className="absolute z-8 ml-[1rem] 
+                mt-[1rem] p-2 rounded-full bg-slate-800 hover:bg-slate-900 
+                transition-colors duration-[.25s] flex items-center justify-center">
+                    <img className="w-[1.5rem] h-[1.5rem]" src={back} alt="Back Icon" 
+                    draggable="false"/>
                 </button>
 
                 {movie.backdrop_path ? (
-                    <img className="opacity-50 w-full h-auto object-cover max-h-[50vh] xl:max-h-[60vh]
-                    2xl:max-h-[65vh]" src={`${TMDB_BACKDROP_BASE_URL}${movie.backdrop_path}`} alt={`${movie.title || movie.name} Backdrop`} />
+                    <img className="opacity-50 w-full h-auto object-cover max-h-[50vh] 
+                    xl:max-h-[60vh] 2xl:max-h-[65vh]" src={`${TMDB_BACKDROP_BASE_URL}${movie.backdrop_path}`} 
+                    alt={`${movie.title || movie.name} Backdrop`} />
                 ) : (
-                    <div className="w-full h-[50vh] bg-slate-900 opacity-50 flex items-center justify-center text-slate-500">No Backdrop Available</div>
+                    <div className="w-full h-[50vh] bg-slate-900 opacity-50 flex 
+                    items-center justify-center text-slate-500">
+                        No Backdrop Available
+                    </div>
                 )}
 
-                <div className="absolute inset-0 bg-gradient-to-t from-[#010617] via-transparent to-transparent"/>
-                <div className="absolute bottom-4 left-0 right-0 px-4 z-5 text-white flex flex-col items-center lg:items-start">
+                <div className="absolute inset-0 bg-gradient-to-t from-[#010617] 
+                via-transparent to-transparent"/>
+                <div className="absolute bottom-4 left-0 right-0 px-4 z-5 text-white flex
+                flex-col items-center lg:items-start">
                     <h1 className="text-xl font-bold text-center mb-2 w-[17ch] 
                     sm:text-2xl sm:w-[25ch] sm:mb-[1rem] md:border-[#b71234] 
-                    lg:mb-[.25rem] lg:w-[auto] lg:ml-[5rem] xl:text-3xl
-                    xl:mb-[.5rem] 2xl:text-4xl 2xl:mb-[1rem]"> {/* Title */}
+                    lg:mb-[.25rem] lg:w-[auto] lg:ml-[5rem] xl:text-3xl xl:mb-[.5rem] 
+                    2xl:text-4xl 2xl:mb-[1rem]">
                         {movie.title || movie.name}
                     </h1>
 
-                    <div className="hidden lg:flex justify-center gap-5 items-center sm:gap-7 lg:ml-[5rem]"> {/* Stats */}
+                    <div className="hidden lg:flex justify-center gap-5 items-center 
+                    mb-[1rem] sm:gap-7 lg:ml-[5rem]">
                         <div className="flex items-center gap-1">
                             <img className="w-4 h-4" src={star} alt="Rating Icon"/>
-                            <h3 className="text-slate-400 text-sm sm:text-base xl:text-lg 2xl:text-xl">{movie.vote_average?.toFixed(1) || "N/A"}</h3>
+                            <h3 className="text-slate-400 text-sm sm:text-base xl:text-lg
+                            2xl:text-xl">
+                                {movie.vote_average?.toFixed(1) || "N/A"}
+                            </h3>
                         </div>
                         <div className="flex items-center gap-1">
                             <img className="w-4 h-4" src={clock} alt="Runtime Icon"/>
-                            <h3 className="text-slate-400 text-sm sm:text-base xl:text-lg 2xl:text-xl">{formatRuntime(movie.runtime || movie.episode_run_time?.[0])}</h3>
+                            <h3 className="text-slate-400 text-sm sm:text-base xl:text-lg
+                            2xl:text-xl">
+                                {formatRuntime(movie.runtime || movie.episode_run_time?.[0])}
+                            </h3>
                         </div>
                         <div className="flex items-center gap-1">
                             <img className="w-4 h-4" src={calendar} alt="Year Icon"/>
-                            <h3 className="text-slate-400 text-sm sm:text-base xl:text-lg 2xl:text-xl">
+                            <h3 className="text-slate-400 text-sm sm:text-base xl:text-lg
+                            2xl:text-xl">
                                 {(movie.release_date || movie.first_air_date || "").substring(0, 4) || "N/A"}
                             </h3>
                         </div>
                     </div>
 
-                    {/* Desktop Overview Prototype */}
-                    <p className="hidden text-slate-400 text-sm leading-relaxed mb-[5rem] lg:block lg:ml-[5rem] w-[70ch] mt-[1rem] 
-                    xl:text-base xl:w-[80ch] xl:mb-[6.5rem] 2xl:text-lg 2xl:w-[88ch] 2xl:mb-[8.5rem]">{movie.overview}</p>
+                    <p className="hidden text-slate-400 text-sm leading-relaxed mb-[5rem]
+                    lg:block lg:ml-[5rem] w-[70ch] mt-[1rem] xl:text-base xl:w-[80ch] 
+                    xl:mb-[6.5rem] 2xl:text-lg 2xl:w-[88ch] 2xl:mb-[8.5rem]">
+                        {movie.overview}
+                    </p>
 
-                    <div className="flex justify-center gap-5 items-center sm:gap-7 lg:hidden"> {/* Stats */}
+                    <div className="flex justify-center gap-5 items-center sm:gap-7 
+                    sm:mb-[2.5rem] lg:hidden">
                         <div className="flex items-center gap-1">
                             <img className="w-4 h-4" src={star} alt="Rating Icon"/>
-                            <h3 className="text-slate-400 text-sm sm:text-base">{movie.vote_average?.toFixed(1) || "N/A"}</h3>
+                            <h3 className="text-slate-400 text-sm sm:text-base">
+                                {movie.vote_average?.toFixed(1) || "N/A"}
+                            </h3>
                         </div>
                         <div className="flex items-center gap-1">
                             <img className="w-4 h-4" src={clock} alt="Runtime Icon"/>
-                            <h3 className="text-slate-400 text-sm sm:text-base">{formatRuntime(movie.runtime || movie.episode_run_time?.[0])}</h3>
+                            <h3 className="text-slate-400 text-sm sm:text-base">
+                                {formatRuntime(movie.runtime || movie.episode_run_time?.[0])}
+                            </h3>
                         </div>
                         <div className="flex items-center gap-1">
                             <img className="w-4 h-4" src={calendar} alt="Year Icon"/>
@@ -173,15 +200,21 @@ function Movie() {
             </div>
 
             <div className="flex justify-center mb-8 px-4 lg:hidden">
-                <button className="bg-slate-800 text-slate-400 text-sm font-[600] inline-flex items-center gap-2 p-3 rounded-[3rem] hover:bg-slate-700 transition-colors">
-                    <img className="w-4 h-4" src={playBtn} alt="Play Icon" draggable="false"/>
+                <button className="bg-slate-800 text-slate-400 text-sm font-[600]
+                inline-flex items-center gap-2 p-3 rounded-[3rem] hover:bg-slate-700
+                 transition-colors">
+                    <img className="w-4 h-4" src={playBtn} alt="Play Icon"
+                    draggable="false"/>
                     Watch Trailer
                 </button>
             </div>
 
             {movie.genres?.length > 0 && (
                 <div className="mx-[2rem] mt-4 lg:mt-[3rem] 2xl:ml-[3.4rem]">
-                    <h1 className="text-white text-lg font-[650] mb-[1rem] md:text-xl lg:text-2xl lg:mb-[1.5rem]">Genres</h1>
+                    <h1 className="text-white text-lg font-[650] mb-[1rem] md:text-xl
+                    lg:text-2xl lg:mb-[1.5rem]">
+                        Genres
+                    </h1>
                     <div className="flex flex-wrap gap-1 md:gap-2 lg:gap-3">
                         {movie.genres.map(genre => (
                             <p key={genre.id} className={GENRES}>{genre.name}</p>
@@ -192,30 +225,47 @@ function Movie() {
 
             {movie.overview && (
                 <div className="mx-[2rem] mt-[2rem] lg:hidden">
-                    <h1 className="text-white text-lg font-[650] mb-[.7rem] md:text-xl">Summary</h1>
-                    <p className="text-slate-400 text-sm leading-relaxed md:text-base">{movie.overview}</p>
+                    <h1 className="text-white text-lg font-[650] mb-[.7rem] md:text-xl">
+                        Summary
+                    </h1>
+                    <p className="text-slate-400 text-sm leading-relaxed md:text-base">
+                        {movie.overview}
+                    </p>
                 </div>
             )}
 
             {cast.length > 0 && (
                 <div className="ml-[2rem] mt-[2rem] 2xl:ml-0 md:mt-[3rem] xl:mt-[4rem]">
-                    <h1 className="text-white text-lg font-[650] mb-[1rem] md:text-xl lg:text-2xl lg:mb-[1.5rem] 2xl:ml-[3.4rem]">Cast</h1>
-                    <div className="flex gap-5 overflow-x-auto whitespace-nowrap custom-scrollbar lg:gap-10 xl:gap-15 2xl:gap-20 2xl:ml-[3.4rem]">
+                    <h1 className="text-white text-lg font-[650] mb-[1rem] md:text-xl
+                    lg:text-2xl lg:mb-[1.5rem] 2xl:ml-[3.4rem]">
+                        Cast
+                    </h1>
+                    <div className="flex gap-5 overflow-x-auto whitespace-nowrap
+                    custom-scrollbar lg:gap-10 xl:gap-15 2xl:gap-20 2xl:ml-[3.4rem]">
                         {cast.map(actor => (
-                            <div key={actor.id} className="flex-shrink-0 text-center w-24 cursor-pointer" onClick={() => navigate(`/actor/${actor.id}`)}>
-                                <div className="mb-2 w-[6rem] h-[8.5rem] bg-slate-800 
-                                rounded-[1rem] overflow-hidden border-[#ffffff00] border-[2.5px]
-                                hover:border-[#b71234] transition-colors duration-[.25s]
-                                md:w-[6rem] md:h-[8.5rem] lg:w-[7rem] lg:h-[10rem] lg:mb-[.7rem] 
-                                xl:mb-[1rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
+                            <div key={actor.id} className="flex-shrink-0 text-center
+                            w-24 cursor-pointer" onClick={() => navigate(`/actor/${actor.id}`)}>
+                                <div className="mb-2 w-[6rem] h-[8.5rem] bg-slate-800
+                                rounded-[1rem] overflow-hidden border-[#ffffff00]
+                                border-[2.5px] hover:border-[#b71234] transition-colors
+                                duration-[.25s] md:w-[6rem] md:h-[8.5rem] lg:w-[7rem]
+                                lg:h-[10rem] lg:mb-[.7rem] xl:mb-[1rem] xl:w-[8rem]
+                                xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
                                     {actor.profile_path ? (
-                                        <img src={`${TMDB_PROFILE_BASE_URL}${actor.profile_path}`} alt={actor.name} className="w-full h-full object-cover" />
+                                        <img src={`${TMDB_PROFILE_BASE_URL}${actor.profile_path}`} 
+                                        alt={actor.name} className="w-full h-full object-cover"/>
                                     ) : (
                                         <span className="text-xs text-slate-400">No Image</span>
                                     )}
                                 </div>
-                                <h3 style={LINE_CLAMP3} className="text-left text-sm text-slate-300 font-[550] mb-1 w-[6rem] xl:text-base xl:w-[8rem]">{actor.name}</h3>
-                                <p style={LINE_CLAMP3} className="text-left text-xs text-slate-500 font-[550] w-[6rem] xl:text-base xl:w-[8rem]">{actor.character}</p>
+                                <h3 style={LINE_CLAMP3} className="text-left text-sm 
+                                text-slate-300 font-[550] mb-1 w-[6rem] xl:text-base xl:w-[8rem]">
+                                    {actor.name}
+                                </h3>
+                                <p style={LINE_CLAMP3} className="text-left text-xs 
+                                text-slate-500 font-[550] w-[6rem] xl:text-base xl:w-[8rem]">
+                                    {actor.character}
+                                </p>
                             </div>
                         ))}
                     </div>
@@ -224,18 +274,24 @@ function Movie() {
 
             {similar.length > 0 && (
                 <div className="ml-[2rem] mt-[2rem] 2xl:ml-0 md:mt-[3rem] xl:mt-[4rem]">
-                    <h1 className="text-white text-lg font-[650] mb-[1rem] md:text-xl lg:text-2xl lg:mt-[3rem] lg:mb-[1.5rem] 2xl:ml-[3.4rem]">More Like This</h1>
-                    <div className="flex gap-5 overflow-x-auto whitespace-nowrap custom-scrollbar lg:gap-10 xl:gap-15 2xl:gap-20 2xl:ml-[3.4rem]">
+                    <h1 className="text-white text-lg font-[650] mb-[1rem] md:text-xl
+                    lg:text-2xl lg:mt-[3rem] lg:mb-[1.5rem] 2xl:ml-[3.4rem]">
+                        More Like This
+                    </h1>
+                    <div className="flex gap-5 overflow-x-auto whitespace-nowrap
+                    custom-scrollbar lg:gap-10 xl:gap-15 2xl:gap-20 2xl:ml-[3.4rem]">
                         {similar.map(similarItem => {
                             const isMovie = Boolean(similarItem.title);
                             const type = isMovie ? "movie" : "tv";
                             return (
-                                <div key={similarItem.id} className="flex-shrink-0 text-center w-24 cursor-pointer" onClick={() => navigate(`/${type}/${similarItem.id}`)}>
+                                <div key={similarItem.id} className="flex-shrink-0 
+                                text-center w-24 cursor-pointer" 
+                                onClick={() => navigate(`/${type}/${similarItem.id}`)}>
                                     <div className="mb-2 w-[6rem] h-[8.5rem] bg-slate-800 
                                     rounded-[1rem] overflow-hidden border-[#ffffff00] border-[2.5px]
                                     hover:border-[#b71234] transition-colors duration-[.25s]
                                     md:w-[6rem] md:h-[8.5rem] lg:w-[7rem] lg:h-[10rem] lg:mb-[.7rem] 
-                                    xl:mb-[1rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
+                                    xl:mb-[1rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]"> {/* Here! */}
                                         {similarItem.poster_path ? (
                                             <img src={`${TMDB_POSTER_BASE_URL}${similarItem.poster_path}`} alt={similarItem.title || similarItem.name} className="w-full h-full object-cover" />
                                         ) : (
