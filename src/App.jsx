56d7f51e0655cc51313e38230d48338cc2589aa9
Raul Luna <mceclipse1889@gmail.com>
2025-08-02 21:17:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from "./HomePage";
+import Results from "./Results.jsx";
 import Movie from "./Movie.jsx";
 import Actor from "./Actor.jsx";
 
@@ -7,6 +8,7 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage/>}/>
+            <Route path="/results" element={<Results/>}/>
             <Route path="/movie/:id" element={<Movie/>}/>
             <Route path="/tv/:id" element={<Movie/>}/>
             <Route path="/actor/:actorId" element={<Actor/>}/>
