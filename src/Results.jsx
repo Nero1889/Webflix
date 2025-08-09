1cc0727061cef5b3b27ac1077780371237f2714a
Raul Luna <mceclipse1889@gmail.com>
2025-08-09 12:07:27 -0500

    Update Results.jsx

--- src/Results.jsx
+++ src/Results.jsx
@@ -238,10 +238,14 @@ function Results() {
                 </div>
             )}
             {error && (
-                <p className="text-[#b71234] text-center mt-[5rem] p-4 mx-5 sm:mx-[3rem]
-                xl:mx-[5rem] 2xl:mx-[7rem]">
-                    {error}
-                </p>
+                <div className="flex flex-col items-center justify-center
+                mt-[5rem] sm:mt-[6.5rem] lg:mt-[7.5rem]">
+                    <img src={warning} alt="Warning Icon" className="w-[5rem]
+                    h-[5rem] sm:w-[6rem] sm:h-[6rem] lg:w-[7rem] lg:h-[7rem]"
+                    draggable="false"/>
+                    <p className="text-[#b71234] text-center font-[550]
+                    mt-[1rem] text-base sm:text-lg lg:text-xl">{error}</p>
+                </div>
             )}
             {searchResults.length > 0 && (
                 <>
