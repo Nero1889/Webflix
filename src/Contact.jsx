26c3aadc5bd34248fa0d6433ba6d28b14a7fdd6f
Raul Luna <mceclipse1889@gmail.com>
2025-08-08 20:31:36 -0500

    Add files via upload

---
+++ src/Contact.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+function Contact() {
+    return (
+        <div>
+            <Header/>
+                <h1 className="text-center text-lg text-white font-[650]">
+                    Contact
+                </h1>
+                <div className="border"></div>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Contact;
